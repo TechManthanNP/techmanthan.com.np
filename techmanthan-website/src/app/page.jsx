6d747f75eb1f7aff64d60a2e1fa48feb2dc74960
pragya-02 <shresthapragya961@gmail.com>
2025-08-02 11:59:16 +0545

    feat: Add productshowcase section

--- techmanthan-website/src/app/page.jsx
+++ techmanthan-website/src/app/page.jsx
@@ -2,6 +2,7 @@
 import Hero from "@/components/Hero";
 import ContactUs from "@/components/homePage/ContactUs";
 import FeaturesSection from "@/components/homePage/FeatureSection";
+import ProductShowcase from "@/components/homePage/ProductShowcase";
 import Subscribe from "@/components/homePage/Subscribe";
 import TestimonialsSection from "@/components/homePage/TestimonialSection";
 import Navbar from "@/components/Navbar";
@@ -15,6 +16,7 @@ export default function Home() {
       <Hero />
       <FeaturesSection />
       <TestimonialsSection />
+      <ProductShowcase />
       <Subscribe />
       <ContactUs />
     </>

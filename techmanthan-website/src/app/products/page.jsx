381d7d55d5ac56be87851c8bafcfeec54c823064
Hasana Manandhar <manandharhasana07@gmail.com>
2025-08-17 13:53:02 +0545

    feat: add products hero section with responsiveness

--- techmanthan-website/src/app/products/page.jsx
+++ techmanthan-website/src/app/products/page.jsx
@@ -1,5 +1,12 @@
+import ProductHero from "@/components/productPage/ProductHero"
+import Navbar from "@/components/Navbar"
 
 export default function ProductsPage() {
-    return <h1>Products</h1>
-  }
-  
+  return (
+    <section>
+      <Navbar></Navbar>
+      {/* <h1>Products</h1> */}
+      <ProductHero></ProductHero>
+    </section>
+  )
+}
---
+++ techmanthan-website/src/components/productPage/ProductHero.jsx
@@ -0,0 +1,48 @@
+"use client"; 
+
+import { motion } from "framer-motion";
+import "../../styles/components/productPage/_productHero.scss";
+
+const textVariants = {
+  hidden: { opacity: 0, y: 50 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 1, ease: "easeOut" },
+  },
+};
+
+const ProductHero = () => {
+  return (
+    <section className="hero-wrapper">
+      <div className="background">
+        <motion.img
+          src="/images/product-hero.gif"
+          alt="hero"
+          initial={{ scale: 1.2, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 1.5, ease: "easeOut" }}
+        />
+        <div className="overlay"></div>
+      </div>
+
+      <motion.div
+        className="hero-content"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: false, amount: 0.3 }} // replay on scroll
+      >
+        <motion.h1 variants={textVariants}>
+          <span>Smarter </span>
+          Systems.
+        </motion.h1>
+
+        <motion.h1 variants={textVariants} transition={{ delay: 0.3 }}>
+          Stronger <span>Outcomes.</span>
+        </motion.h1>
+      </motion.div>
+    </section>
+  );
+};
+
+export default ProductHero;

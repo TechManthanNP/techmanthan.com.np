5a564e04d1e49fef095726a69b5dd8867e4b2e18
pragya-02 <shresthapragya961@gmail.com>
2025-08-26 18:04:32 +0545

    feat: Add responsive design for Internship sub-page and Career module

--- techmanthan-website/src/app/career/internships/page.jsx
+++ techmanthan-website/src/app/career/internships/page.jsx
@@ -1,5 +1,15 @@
 import DigitalExhibition from "@/components/arcPage/DigitalExhibition";
+import ArcProgram from "@/components/careerPage/internPage/ArcProgram";
+import KnowledgeHub from "@/components/careerPage/internPage/Knowledge";
+import Testimonials from "@/components/careerPage/internPage/Testomonials";
 
 export default function InternshipsPage() {
-  return <DigitalExhibition />;
+  return (
+    <>
+      <ArcProgram />
+      <DigitalExhibition />
+      <KnowledgeHub />
+      <Testimonials />
+    </>
+  );
 }
